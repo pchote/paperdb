ba528e8b8a32a54e45ffd242764c92c5b4a4445f
Paul Chote <p.chote@warwick.ac.uk>
2018-10-09 18:10:08 +0100

    Replace bootstrap table with DataTables.

--- static/paperdb.js
+++ static/paperdb.js
@@ -1,13 +1,13 @@
-function buildnames(value, row, index) {
+function buildnames(data, type, row, meta) {
   // Only display the first author, but include all authors so they can be indexed in the search field
-  if (value instanceof Array)
-    return value[0] + (value.length > 1 ? ' et. al.' : '') + '<span style="display: none">' + value.join(' ') + '</span>';
-  return value;
+  if (data instanceof Array)
+    return data[0] + (data.length > 1 ? ' et. al.' : '') + '<span style="display: none">' + data.join(' ') + '</span>';
+  return data;
 }
 
-function buildtitle(value, row, index) {
+function buildtitle(data, type, row, meta) {
   // Include the abstract and keywords so they can be indexed by the search field
-  return value + '<span style="display: none">' + row['abstract'] + row['keywords'] + '</span>';
+  return data + '<span style="display: none">' + row['abstract'] + row['keywords'] + '</span>';
 }
 
 function bind_paper_button(button, url) {
@@ -18,8 +18,22 @@ function bind_paper_button(button, url) {
     button.hide();
 }
 
-$(function() {
-  $('#table').on('click-row.bs.table', function(row, element, field) {
+function setup(data_url) {
+  var table = $('#table').DataTable( {
+    ajax: { url: data_url, dataSrc: '' },
+    dom: "<'row'<'col-sm-6'f><'col-sm-6'p>><'row'<'col-sm-12'tr>><'row'<'col-sm-6'l><'col-sm-6'p>>",
+    lengthMenu: [[10, 25, 50, -1], [10, 25, 50, "All"]],
+    columns: [
+      { data: 'author', width: '17%', render: buildnames },
+      { data: 'year', width: '8%' },
+      { data: 'title', width: '50%', render: buildtitle },
+      { data: 'journal', width: '25%' },
+    ]
+  });
+
+  $('#table tbody').on('click', 'tr', function () {
+    var element = table.row( this ).data();
+
     $('#detailmodal').modal();
     $('#paper-title').text(element.title);
     $('#paper-author').html(element.author.join(' and '));
@@ -41,8 +55,4 @@ $(function() {
     bind_paper_button($('#paper-arxiv'), element.arxiv);
     bind_paper_button($('#paper-web'), element.web);
   });
-
-  $('#table').on('load-error.bs.table', function() {
-    $('#table').bootstrapTable('updateFormatText', 'NoMatches', 'There are no papers to display. Have you logged in?');
-  });
-});
+}
